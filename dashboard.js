1ed5dec81f8ef064079489605c80b94308ea9d18
SRI SUDHARSHANA S <srisudharshanas2406@gmail.com>
2025-12-21 14:23:15 +0530

    Replace script URL with Google Sheets URL

--- dashboard.js
+++ dashboard.js
@@ -1,16 +1,9 @@
+const sheetUrl =
+  "https://docs.google.com/spreadsheets/d/1gqB3-KYs2ca_1yqEoWaLsIZIDODC-1erskSdUJrPE1k/edit";
 
-const scriptUrl = "https://script.google.com/macros/s/AKfycbyJCWn1fvwuukE8Dg1ilQTs9hcFubwlpeEGEq4PYIS22elDsUKJdzjhJqnFccwXL2FpJQ/exec";
-
-document.getElementById("openSheetBtn").addEventListener("click", function() {
-  const newWindow = window.open(scriptUrl, "_blank");
-  if (!newWindow) {
-    document.getElementById("errorMsg").textContent = 
-      "Pop-up blocked! Please allow pop-ups in your browser.";
-  } else {
-    document.getElementById("errorMsg").textContent = "";
-  }
+document.getElementById("openSheetBtn").addEventListener("click", () => {
+  window.open(sheetUrl, "_blank");
 });
-
 
 
 
