b7f4051cd9c9301f80ff2f03d378a1def10c7ea6
SRI SUDHARSHANA S <srisudharshanas2406@gmail.com>
2025-12-21 11:56:28 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,10 +1,8 @@
 function openSheet() {
   window.open(
-    "https://docs.google.com/spreadsheets/d/AKfycbxiqRFNgSgLnu41HhG3AnAOW4aiUxgIvD1Jfs72UvH3_51r7xTrL5id1XYw6defysy5xw",
+    "https://docs.google.com/spreadsheets/d/AKfycbxiqRFNgSgLnu41HhG3AnAOW4aiUxgIvD1Jfs72UvH3_51r7xTrL5id1XYw6defysy5xw/edit",
     "_blank"
   );
 }
-
-
 
 
