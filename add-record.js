08f523c2c7a9792d52b179907b8eba0f5b205576
SRI SUDHARSHANA S <srisudharshanas2406@gmail.com>
2025-12-21 18:48:57 +0530

    Update add-record.js

--- add-record.js
+++ add-record.js
@@ -1,38 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("patientForm");
-
-  
-  const scriptURL = "https://script.google.com/macros/s/AKfycbwV-Ih6TgJngY09X8-8tsw8wNLXqe1kUrtL9U38VPId6cMLHiUUAmukJScv3i-fGKhk1A/exec";
+  const scriptURL = "https://script.google.com/macros/s/AKfycbx79rWGdf7wGq7WuZA6z9gJaNBtupMa8Hw2RO6dABPCYcJmjFL5Nbbp8Q3OFfhJjoXQ1A/exec";
 
   form.addEventListener("submit", function (e) {
     e.preventDefault();
 
+    
     const formData = new FormData(form);
-    let data = {};
-
-    formData.forEach((value, key) => {
-      
-      if (key !== "medical_files") data[key] = value;
-    });
-
     
     fetch(scriptURL, {
       method: "POST",
       mode: "no-cors", 
-      cache: "no-cache",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
+      body: formData
     })
     .then(() => {
-      
-      alert("Record sent to Google Sheets!");
+      alert("Record submitted successfully!");
       form.reset();
     })
-    .catch((err) => {
+    .catch(err => {
       console.error("Error:", err);
-      alert("Something went wrong. Check console.");
+      alert("Submission failed. Check your internet or Script Deployment.");
     });
   });
 });
