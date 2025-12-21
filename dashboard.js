d7094b675bc3b4ae481c69effe651d155bdfff73
SRI SUDHARSHANA S <srisudharshanas2406@gmail.com>
2025-12-21 12:24:28 +0530

    Fix URL in openSheet function

--- dashboard.js
+++ dashboard.js
@@ -1,9 +1,10 @@
 function openSheet() {
   window.open(
-    "https://docs.google.com/spreadsheets/d/https://script.google.com/macros/s/https://script.google.com/macros/library/d/1acpJkSULaSE4tX3QRmkrXsQayxJ33FJfGRWFH6SlCPq-o3BL2ONoDT5l/1/edit",
+    "https://docs.google.com/spreadsheets/d/https://script.google.com/macros/s/1acpJkSULaSE4tX3QRmkrXsQayxJ33FJfGRWFH6SlCPq-o3BL2ONoDT5l/edit",
     "_blank"
   );
 }
+
 
 
 
