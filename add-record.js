ca33bfd8f3dddf3fbc78b468c2ec9303a3693a46
SRI SUDHARSHANA S <srisudharshanas2406@gmail.com>
2025-12-21 15:50:31 +0530

    Update add-record.js

--- add-record.js
+++ add-record.js
@@ -1,16 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-
   const form = document.getElementById("patientForm");
-
-  
-  const scriptURL = "https://script.google.com/macros/s/AKfycbzNnRPjrkiNg3K0C8laTfC6wUMt9PFFM14586LoDuHek2TlXbtNrBSfrKrO0lyzspz1fQ/exec";
 
   form.addEventListener("submit", function (e) {
     e.preventDefault();
 
     const formData = new FormData(form);
     let data = {};
-
     formData.forEach((value, key) => {
       if (key !== "medical_files") data[key] = value;
     });
@@ -20,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
       body: JSON.stringify(data),
       headers: { "Content-Type": "application/json" }
     })
-    .then(response => response.json()) 
+    .then(response => response.json())
     .then(res => {
       if (res.status === "success") {
         alert("Record submitted successfully!");
@@ -34,12 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
       alert("Failed to submit record. Check Apps Script deployment and permissions.");
     });
   });
-
 });
-
-
-
-
 
 
 
