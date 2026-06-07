c52401fb636a6801e2b852a3f266a687cf4d4dde
SRI SUDHARSHANA S <srisudharshanas2406@gmail.com>
2026-06-07 13:07:39 +0530

    Update add-record.js

--- add-record.js
+++ add-record.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("patientForm");
 
   const scriptURL =
-"https://script.google.com/macros/s/AKfycbz4MGPFA_qdPuFMyn04_524T_rXId6KebEKIvfWFUXc-wyU-r4jObBQS960T7HcrxY9/exec"
+"https://script.google.com/macros/s/AKfycbz4MGPFA_qdPuFMyn04_524T_rXId6KebEKIvfWFUXc-wyU-r4jObBQS960T7HcrxY9/exec";
 
   form.addEventListener("submit", async (e) => {
 
@@ -11,14 +11,51 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const formData = new FormData(form);
 
+    const startDate =
+      formData.get("start_date");
+
+    const endDate =
+      formData.get("end_date");
+
+    if(startDate > endDate){
+
+      alert("End Date must be after Start Date");
+
+      return;
+
+    }
+
+    const stock =
+      Number(formData.get("stock"));
+
+    if(stock < 0){
+
+      alert("Stock cannot be negative");
+
+      return;
+
+    }
+
     const reminder = {
-      medicine: formData.get("medicine_name"),
-      time: formData.get("reminder_time"),
-      dosage: formData.get("dosage")
+
+      medicine:
+        formData.get("medicine_name"),
+
+      dosage:
+        formData.get("dosage"),
+
+      time:
+        formData.get("reminder_time"),
+
+      frequency:
+        formData.get("frequency")
+
     };
 
     const reminders =
-      JSON.parse(localStorage.getItem("mt_reminders")) || [];
+      JSON.parse(
+        localStorage.getItem("mt_reminders")
+      ) || [];
 
     reminders.push(reminder);
 
@@ -29,21 +66,31 @@ document.addEventListener("DOMContentLoaded", () => {
 
     try {
 
-      await fetch(scriptURL,{
-        method:"POST",
-        mode:"no-cors",
-        body:formData
+      await fetch(scriptURL, {
+
+        method: "POST",
+
+        mode: "no-cors",
+
+        body: formData
+
       });
 
-      alert("Medication saved successfully");
+      alert(
+        "Medication Record Saved Successfully"
+      );
 
       form.reset();
 
-    } catch(error){
+    }
+
+    catch(error){
 
       console.error(error);
 
-      alert("Unable to save record");
+      alert(
+        "Unable to save record"
+      );
 
     }
 
