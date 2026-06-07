6e3bc2a9648d7b9c94509597083e000889c6609e
SRI SUDHARSHANA S <srisudharshanas2406@gmail.com>
2026-06-07 12:09:11 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,55 +1,6 @@
-document.addEventListener("DOMContentLoaded",()=>{
+const sheetUrl =
+  "https://docs.google.com/spreadsheets/d/1gqB3-KYs2ca_1yqEoWaLsIZIDODC-1erskSdUJrPE1k/edit";
 
-  const reminders =
-    JSON.parse(
-      localStorage.getItem("mt_reminders")
-    ) || [];
-
-  document.getElementById("activeMeds").textContent =
-    reminders.length;
-
-  document.getElementById("todayDoses").textContent =
-    reminders.length;
-
-  let lowStockCount = 0;
-
-  const records =
-    JSON.parse(
-      localStorage.getItem("mt_records")
-    ) || [];
-
-  records.forEach(record=>{
-
-    const stock =
-      Number(record.stock);
-
-    const daily =
-      Number(record.daily_consumption);
-
-    if(daily > 0){
-
-      const daysLeft =
-      stock/daily;
-
-      if(daysLeft <= 7){
-
-        lowStockCount++;
-
-      }
-
-    }
-
-  });
-
-  document.getElementById("lowStock").textContent =
-    lowStockCount;
-
+document.getElementById("openSheetBtn").addEventListener("click", () => {
+  window.open(sheetUrl, "_blank");
 });
-
-
-
-
-
-
-
-
